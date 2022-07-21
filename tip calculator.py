93f611ddf17c0fe80fd758bfa8f56e59af7a88cd
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-21 15:20:13 +0200

    Update tip calculator.py

--- tip calculator.py
+++ tip calculator.py
@@ -5,5 +5,6 @@ percentage = float (input (f"Enter the tip amount - 10, 12 or 15 (%): ") )
 percentage_format = 1 + percentage * 0.01  #  or w/o this and instead: total_bill = percentage * bill + bill
 
 total_per_person = bill / number_of_people * percentage_format
-total_per_person_format = "{:.2f}".format (total_per_person)
+total_per_person_format = round(total_per_person,2)
+#  OR:                  = "{:.2f}".format(total_per_person)
 print(f"The bill and the {percentage} % tip split {number_of_people} ways is: {total_per_person_format}")
